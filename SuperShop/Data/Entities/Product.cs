1868a94bfb6e6a7d75181c6b458aa9ec7c665899
Álvaro Neves <alvaro.neves2019@gmail.com>
2025-04-29 16:37:39 +0100

    Modify Products

--- SuperShop/Data/Entities/Product.cs
+++ SuperShop/Data/Entities/Product.cs
@@ -6,7 +6,8 @@ namespace SuperShop.Data.Entities
     public class Product
     {
         public int Id { get; set; }
-
+        [Required]
+        [MaxLength(50, ErrorMessage = "The field {0} can contain {1} characters length.")]
         public string Name { get; set; }
 
         [DisplayFormat(DataFormatString = "{0:C2}", ApplyFormatInEditMode = false)]
@@ -18,11 +19,11 @@ namespace SuperShop.Data.Entities
 
 
         [Display(Name = "Last Purchase")]
-        public DateTime LastPurchase { get; set; }
+        public DateTime? LastPurchase { get; set; }
 
 
         [Display(Name = "Last Sale")]
-        public DateTime LastSale { get; set; }
+        public DateTime? LastSale { get; set; }
 
 
         [Display(Name = "is Available")]
